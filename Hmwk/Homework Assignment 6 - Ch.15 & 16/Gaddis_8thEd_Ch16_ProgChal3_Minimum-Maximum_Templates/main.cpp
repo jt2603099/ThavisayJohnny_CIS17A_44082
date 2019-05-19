fa2191d68722e041cd454db91e5a5f911bb9466c
jt2603099 <jthavisay@student.rccd.edu>
2019-05-18 23:55:48 -0700

    Signed-off-by: jt2603099 <jthavisay@student.rccd.edu>

--- Hmwk/Homework Assignment 6 - Ch.15 & 16/Gaddis_8thEd_Ch16_ProgChal3_Minimum-Maximum_Templates/main.cpp
+++ Hmwk/Homework Assignment 6 - Ch.15 & 16/Gaddis_8thEd_Ch16_ProgChal3_Minimum-Maximum_Templates/main.cpp
@@ -13,11 +13,55 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
+//Function template prototypes
+template <class T>
+T minimum(T val1, T val2);
+
+template <class T>
+T maximum(T val1, T val2);
+
 int main(int argc, char** argv) {
+    char test1 = 'A', test2 = 'E';
+    int test3 = 2, test4 = 10;
+    double test5 = 2.1, test6 = 7.2;
+    string test7 = "BAD", test8 = "BAO";
+    
+    //char
+    cout << "A and E" <<endl;
+    cout << "Smaller: " << minimum(test1, test2) <<endl;
+    cout << "Larger: " << maximum(test1, test2) <<endl <<endl;
+    //int
+    cout << test3 << " and " << test4 <<endl;
+    cout << "Smaller: " << minimum(test3, test4) <<endl;
+    cout << "Larger: " << maximum(test3, test4) <<endl <<endl;
+    //double
+    cout << test5 << " and " << test6 <<endl;
+    cout << "Smaller: " << minimum(test5, test6) <<endl;
+    cout << "Larger: " << maximum(test5, test6) <<endl <<endl;
+    //string
+    cout << test7 << " and " << test8 <<endl;
+    cout << "Smaller: " << minimum(test7, test8) <<endl;
+    cout << "Larger: " << maximum(test7, test8) <<endl <<endl;
 
     return 0;
 }
 
+template <class T>
+T minimum(T val1, T val2) {
+    if (val1 < val2) {
+        return val1;
+    }
+    return val2;
+}
+
+template <class T>
+T maximum(T val1, T val2) {
+    if (val1 > val2) {
+        return val1;
+    }
+    return val2;
+}
--- Hmwk/Homework Assignment 6 - Ch.15 & 16/Gaddis_8thEd_Ch16_ProgChal5_TotalTemplate/main.cpp
+++ Hmwk/Homework Assignment 6 - Ch.15 & 16/Gaddis_8thEd_Ch16_ProgChal5_TotalTemplate/main.cpp
@@ -13,11 +13,40 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <iomanip> //setprecision
 
 using namespace std;
 
-int main(int argc, char** argv) {
+//function class prototype
+template <class T>
+T total(int numberOfValues);
 
+int main(int argc, char** argv) {
+    
+    //3 ints
+    cout << "Total: " << total<int>(3) <<endl;
+    
+    //4 char
+    cout << "Total: " << total<float>(4) <<endl;
+    
+    //5 double
+    cout << fixed << setprecision(2);
+    cout << "Total: " << total<double>(5) <<endl;
+    
+    //End program
     return 0;
 }
 
+//total function class definition
+template <class T>
+T total(int numberOfValues) {
+    T sum = 0, value;
+    
+    while (numberOfValues > 0) {
+        cin >> value;
+        sum += value;
+        //Decrement counter
+        numberOfValues--;
+    }
+    return sum;
+}
